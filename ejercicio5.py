8f087f54f909d5d38fda5f57155ef73378b43963
Alondra <asmayen@uamv.edu.ni>
2025-04-08 10:52:59 -0600

    ejercicio5.py

--- ejercicio5.py
+++ ejercicio5.py
@@ -2,8 +2,15 @@
 # (a + b) * c / 2.
 # Muestra el resultado y explica cómo se evalúa la expresión paso a paso.
 
-variable1 = 5
-variable2 = 8
-variable3 = 10
+a = int(input ("Introduzca la primer variable:"))
+b = int(input ("Introduzca la segunda variable:"))
+c = int(input ("Introduzca la tercer variable:"))
 
-print 
+resultado = (a + b) * c / 2.
+
+print ("El resultado de la expresion de esta variable es: ", resultado)
+print ("La expresion se evalua de la siguiente forma:" )
+print ("1- Se suman las variables a y b")
+print ("2- El resultado de la suma se multiplica por c")
+print ("3- El resultado de la multiplicacion se divide entre 2")
+print ("4- Ya tienes el resultado de esta expresion")
