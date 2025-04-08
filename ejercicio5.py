e6a7bfdf9864fb759fd3f13635065d67b63fdf80
AlondraMayen <asmayen@uamv.edu.ni>
2025-04-08 10:21:59 -0600

    semana4

---
+++ ejercicio5.py
@@ -0,0 +1,9 @@
+# Declara tres variables numéricas y evalúa la siguiente expresión:
+# (a + b) * c / 2.
+# Muestra el resultado y explica cómo se evalúa la expresión paso a paso.
+
+variable1 = 5
+variable2 = 8
+variable3 = 10
+
+print 
