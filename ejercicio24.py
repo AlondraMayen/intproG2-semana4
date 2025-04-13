033adc0ed225c26794aa92b5027458c33c49864c
Alondra <asmayen@uamv.edu.ni>
2025-04-13 03:37:47 -0600

    Delete ejercicio24.py

--- ejercicio24.py
+++
@@ -1,9 +0,0 @@
-presupuesto_total = float(input("Ingrese el monto total del presupuesto: "))
-
-urgencias = presupuesto_total * 0.37
-pediatria = presupuesto_total * 0.42
-traumatologia = presupuesto_total * 0.21
-
-print("Urgencias recibe:", urgencias)
-print("Pediatría recibe:", pediatria)
-print("Traumatología recibe:", traumatologia)
