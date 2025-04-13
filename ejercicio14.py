b930c18f5b767611fe46cb6b140ef09e4eee0f33
Alondra <asmayen@uamv.edu.ni>
2025-04-13 03:34:45 -0600

    Delete ejercicio14.py

--- ejercicio14.py
+++
@@ -1,8 +0,0 @@
-nombre_producto = input("Ingrese el nombre del producto: ")
-precio_producto = float(input("Ingrese el precio del producto: "))
-porcentaje_descuento = float(input("Ingrese el porcentaje de descuento: ")) / 100
-
-precio_final = precio_producto - (precio_producto * porcentaje_descuento)
-
-print("Producto:", nombre_producto)
-print("Precio final:", precio_final)
