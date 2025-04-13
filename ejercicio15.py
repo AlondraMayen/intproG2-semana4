826c8b52f5887627db44034496ef2b337dc7ab99
Alondra <asmayen@uamv.edu.ni>
2025-04-13 03:34:57 -0600

    Delete ejercicio15.py

--- ejercicio15.py
+++
@@ -1,6 +0,0 @@
-total_cuenta = float(input("Ingrese el total de la cuenta: "))
-porcentaje_propina = float(input("Ingrese el porcentaje de propina: ")) / 100
-
-propina = total_cuenta * porcentaje_propina
-
-print("Propina a dejar:", propina)
