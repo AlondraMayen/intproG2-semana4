95f29a74f58a0c865abf286ce47270e8ba6e6aca
Alondra <asmayen@uamv.edu.ni>
2025-04-13 03:37:36 -0600

    Delete ejercicio23.py

--- ejercicio23.py
+++
@@ -1,7 +0,0 @@
-lunes = float(input("Tiempo del lunes (en minutos): "))
-miercoles = float(input("Tiempo del miércoles (en minutos): "))
-viernes = float(input("Tiempo del viernes (en minutos): "))
-
-promedio_tiempo = (lunes + miercoles + viernes) / 3
-
-print("Tiempo promedio de recorrido en la semana:", promedio_tiempo, "minutos")
