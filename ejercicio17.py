46e80666eca6346716a95031053ec11352a12b45
Alondra <asmayen@uamv.edu.ni>
2025-04-13 03:36:11 -0600

    Delete ejercicio17.py

--- ejercicio17.py
+++
@@ -1,7 +0,0 @@
-tareas = float(input("Ingrese la nota de tareas (sobre 100): "))
-examen_parcial = float(input("Ingrese la nota del examen parcial (sobre 100): "))
-examen_final = float(input("Ingrese la nota del examen final (sobre 100): "))
-
-calificacion_final = (tareas * 0.30) + (examen_parcial * 0.30) + (examen_final * 0.40)
-
-print("Calificación final:", calificacion_final)
