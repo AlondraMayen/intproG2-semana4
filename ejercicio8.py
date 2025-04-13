93ef7ef2f0fb8e41514153ce9dc74fc8b54149bd
AlondraMayen <asmayen@uamv.edu.ni>
2025-04-13 04:11:07 -0600

    semana 4

---
+++ ejercicio8.py
@@ -0,0 +1,7 @@
+# Algoritmo que calcule el porcentaje de un número dado. Ejemplo: ¿qué es el 15% de 200?
+
+numero = int(input("Introduzca el numero:"))
+porcentaje = int(input("Introduzca el porcentaje:"))
+resultado = (numero*porcentaje) /100
+
+print ("El porcentaje es de:", resultado)
