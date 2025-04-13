bcbb33aee38a3f7ec241821e20f3b09c7e24d654
Alondra <asmayen@uamv.edu.ni>
2025-04-13 03:34:32 -0600

    Delete ejercicio13.py

--- ejercicio13.py
+++
@@ -1,6 +0,0 @@
-salario_actual = float(input("Ingrese el salario actual: "))
-incremento = salario_actual * 0.08
-descuento = salario_actual * 0.025
-nuevo_salario = salario_actual + incremento - descuento
-
-print("Nuevo salario:", nuevo_salario)
