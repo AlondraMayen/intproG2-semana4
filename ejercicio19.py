5df8db4821d3c044fa87e8b270322c18fbd614e8
AlondraMayen <asmayen@uamv.edu.ni>
2025-04-13 04:19:10 -0600

    semana 4

---
+++ ejercicio19.py
@@ -0,0 +1,9 @@
+edad_actual = int(input("Ingrese su edad actual: "))
+
+edad_5 = edad_actual + 5
+edad_10 = edad_actual + 10
+edad_20 = edad_actual + 20
+
+print("Edad en 5 años:", edad_5)
+print("Edad en 10 años:", edad_10)
+print("Edad en 20 años:", edad_20)
