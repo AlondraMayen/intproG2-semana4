a1a265b746a68892ecd5d5fd5fd8a2688e6ea088
AlondraMayen <asmayen@uamv.edu.ni>
2025-04-13 03:51:32 -0600

    semana4

--- ejercicio6.py
+++
@@ -1,8 +0,0 @@
-# Escribe un algoritmo que reciba el nombre y apellido de una persona, y luego muestre el nombre completo concatenado.
-
-nombres = input("Introduzca su primer y segundo nombre:")
-apellidos = input("Introduzca su primer y segundo apellido:")
-
-nombre_completo = nombres + apellidos
-
-print("Su nombre completo es:", nombre_completo)
